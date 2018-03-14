b57e42a2686865d9a078471eeb1675c98940ddd2
OttoOctavius <OttoOctavius@users.noreply.github.com>
2018-03-14 16:50:36 -0300

    Intentando refactorizar
    Implementando el guardado

--- InterpretePLC.hs
+++ InterpretePLC.hs
@@ -1,4 +1,7 @@
-
+{-Colocar todas las instrucciones en un dato algebraico.
+  Las variables son las que pueden estar negadas o no. Ademas establece el nombre y tipo
+  de la compuerta.
+  Al final, instrucciones para almacenar resultados.  -}
 data Instruccion =   LD Var (Instruccion) |
                      OR Var (Instruccion) |
                      AND Var (Instruccion)|
@@ -18,3 +21,14 @@ eval (OR var inst) = var : (eval inst)
                               case Map.lookup n env of
                                 Nothing -> throwError "Falta esa variable"
                                 Just val -> return val
+
+guardar (SET v) = \b-> do env <- ask
+                          case Map.lookup n env of
+                            Nothing -> throwError "type error"
+                            Just val -> tell val
+guardar (RST v) = \b-> do if b == false then
+                            env <- ask
+                            case Map.lookup n env of
+                              Nothing -> throwError "type error"
+                              Just val -> tell val
+                          else Nothing
