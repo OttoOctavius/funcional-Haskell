9ce08eff36a4eb984b5a941c3399329bdd00e03e
OttoOctavius <OttoOctavius@users.noreply.github.com>
2018-08-15 23:30:46 -0300

    el show

--- InterpreteScheme/helloworld.hs
+++ InterpreteScheme/helloworld.hs
@@ -66,6 +66,19 @@ module Main where
                     char ')'
                     return x
 
+
+
+    showVal :: LispVal -> String
+    showVal (String contents) = "\"" ++ contents ++ "\""
+    showVal (Atom name) = name
+    showVal (Number contents) = show contents
+    showVal (Bool True) = "#t"
+    showVal (Bool False) = "#f"
+    showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+    showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+        where unwordsList = unwords . map showVal
+
+
     main :: IO ()
     main = do 
             (expr:_) <- getArgs
