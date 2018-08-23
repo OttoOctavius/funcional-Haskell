25ed82634847ee256caa85a52d5d19729326d18a
OttoOctavius <OttoOctavius@users.noreply.github.com>
2018-08-22 22:47:06 -0300

    Corregidos errores menores

--- Ejercicios_intermedios/Euler.hs
+++ Ejercicios_intermedios/Euler.hs
@@ -1,13 +1,12 @@
-import Data.List (\\)  -- (\\) is set-difference for unordered lists
+import Data.List  -- (\\) is set-difference for unordered lists
 
 sumaDeMultiplosDe3o5Hasta1000  = sum (multiplosDe 3 5)
     where multiplosDe n m = [x | x <- [1..1000], x  `mod` n==0 || x `mod` m==0 ]
-
-
-{-Numeros primos, para el numero 600851475143 -}
-sumadeprimosDe600851475143 = sum [ x | x <- primeTo 600851475143]
 
 primesTo m = ps 
              where
              ps = map head $ takeWhile (not.null) 
                            $ scanl (\\) [2..m] [[p, p+p..m] | p <- ps]
+{-Numeros primos, para el numero 600851475143 -}
+sumadeprimosDe600851475143 = sum [ x | x <- primesTo 600851475143]
+
