98fb7b82965f6e356ea1cac8f3f6b7ce85a6007d
OttoOctavius <OttoOctavius@users.noreply.github.com>
2018-08-24 23:22:22 -0300

    Ejercicio de Palindromos
    Se hacen productos y convierten enteros a Strings, para procesarlos con isPalindrome

--- Ejercicios_intermedios/Euler.hs
+++ Ejercicios_intermedios/Euler.hs
@@ -12,3 +12,7 @@ sumadeprimosDe600851475143 = max [ x | x <- primesTo 600851475143]
 
 fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
 sumaEvenfibonassi = sum [x | x <- fibs, x < 4000000 && even x ]
+
+isPalindrome''' = Control.Monad.liftM2 (==) id reverse
+palindromosDe3Cifras = [x*y| x <- [100..999], y <- [100..999] , isPalindrome''' $show(x*y)]
+mayorPalindromo = max palindromosDe3Cifras
