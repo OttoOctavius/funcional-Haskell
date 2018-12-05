34767bd6ccbff3a2b3fd4bb9d251c1c5a16fc59d
OttoOctavius <OttoOctavius@users.noreply.github.com>
2018-12-05 00:56:15 -0300

    Intentando updates
    Viendo como hacer modificaciones

--- Ejercicios_intermedios/Datos.hs
+++ Ejercicios_intermedios/Datos.hs
@@ -1,7 +1,18 @@
 import Data.Array --http://hackage.haskell.org/package/array-0.5.2.0/docs/Data-Array.html
+import Control.Monad --Para usar el forM_
+import Control.Monad.LoopWhile
 
-main = let xs = array (0,10) [(i, i * i) | i <- [0..10]]
-        in do
-                putStrLn $ "Limites son: " ++ show (bounds xs)
-                putStrLn $ "Indices son: " ++ show (indices xs)
-                putStrLn $ "Elementos son: " ++ show (elems xs) 
+--Este tipo de definicion de array permite recursion sobre "xs"
+accesos = let xs = array (0,10) [(i, i * i) | i <- [0..10]]
+     in do
+        putStrLn $ "Limites son: " ++ show (bounds xs)
+        putStrLn $ "Indices son: " ++ show (indices xs)
+        putStrLn $ "Elementos son: " ++ show (elems xs)
+        putStrLn $ "Primer elemento: " ++ show ( assocs xs !!0)
+        forM_ [0..10] (\x -> putStrLn $ show (xs!x))
+
+incremUpdates = let lista = listArray (0,2) [10,20,30]
+        in loop $ do return lista
+                while (lista!0 < 0)
+
+main = incremUpdates --accesos
