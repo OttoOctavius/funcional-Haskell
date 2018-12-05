afad72d51fe454b4605a4fc449db012056e3cdce
OttoOctavius <OttoOctavius@users.noreply.github.com>
2018-12-04 23:51:44 -0300

    Update Datos.hs
    Se utilizan otras funciones

--- Ejercicios_intermedios/Datos.hs
+++ Ejercicios_intermedios/Datos.hs
@@ -1,6 +1,7 @@
-import Data.Array
+import Data.Array --http://hackage.haskell.org/package/array-0.5.2.0/docs/Data-Array.html
 
 main = let xs = array (0,10) [(i, i * i) | i <- [0..10]]
         in do
-                putStr $ "Limites son: " ++ show (bounds xs)
-                
+                putStrLn $ "Limites son: " ++ show (bounds xs)
+                putStrLn $ "Indices son: " ++ show (indices xs)
+                putStrLn $ "Elementos son: " ++ show (elems xs) 
