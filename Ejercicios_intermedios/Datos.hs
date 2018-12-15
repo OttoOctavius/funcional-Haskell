ea5d8de1d757abe68e6c490ead89f7767ef02bc5
OttoOctavius <OttoOctavius@users.noreply.github.com>
2018-12-15 00:00:09 -0300

    Probando vectores

--- Ejercicios_intermedios/Datos.hs
+++ Ejercicios_intermedios/Datos.hs
@@ -1,6 +1,13 @@
 import Data.Array --http://hackage.haskell.org/package/array-0.5.2.0/docs/Data-Array.html
 import Control.Monad --Para usar el forM_
-import Control.Monad.LoopWhile
+--import Control.Monad.LoopWhile
+import Data.Vector ((!), generate)
+ 
+
+buyable n = r!n
+  where
+    r = generate (n+1) f
+    f i = i == 0 || i >= 6 && r!(i-6) || i >= 9 && r!(i-9) || i >= 20 && r!(i-20)
 
 --Este tipo de definicion de array permite recursion sobre "xs"
 accesos = let xs = array (0,10) [(i, i * i) | i <- [0..10]]
@@ -11,8 +18,10 @@ accesos = let xs = array (0,10) [(i, i * i) | i <- [0..10]]
         putStrLn $ "Primer elemento: " ++ show ( assocs xs !!0)
         forM_ [0..10] (\x -> putStrLn $ show (xs!x))
 
+        {-
 incremUpdates = let lista = listArray (0,2) [10,20,30]
         in loop $ do return lista
                 while (lista!0 < 0)
+        -}
 
-main = incremUpdates --accesos
+main = putStrLn $ buyable 10
